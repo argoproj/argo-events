3d4b06b5024727d54ac89e5657e5035b07ce3223
Tayfun Wiechert <tayfun.wiechert@gmail.com>
2018-11-26 14:54:54 +0100

    gateway deployment template inherits annotation of gateway spec (#129)
    * gateway deployment template inherits annotation of gateway spec
    
    * gateway and sensor deployment template inherits annotation of gateway/sensor spec

--- controllers/sensor/operator.go
+++ controllers/sensor/operator.go
@@ -142,8 +142,10 @@ func (soc *sOperationCtx) operate() error {
 						},
 						Template: corev1.PodTemplateSpec{
 							ObjectMeta: metav1.ObjectMeta{
-								Name:      common.DefaultSensorDeploymentName(soc.s.Name),
-								Namespace: soc.s.Name,
+								Name:        common.DefaultSensorDeploymentName(soc.s.Name),
+								Namespace:   soc.s.Name,
+								Annotations: soc.s.Annotations,
+
 								Labels: map[string]string{
 									common.LabelSensorName: soc.s.Name,
 								},
@@ -187,6 +189,7 @@ func (soc *sOperationCtx) operate() error {
 						Template: corev1.PodTemplateSpec{
 							ObjectMeta: metav1.ObjectMeta{
 								GenerateName: common.DefaultSensorJobName(soc.s.Name),
+								Annotations:  soc.s.Annotations,
 								Labels: map[string]string{
 									common.LabelSensorName: soc.s.Name,
 								},
