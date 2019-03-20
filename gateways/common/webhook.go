argoproj/argo-events

commit 2921f78c5448c4146a6505755229714bf42473d1

gateway/github: Enterprise support and some fixes (#221)
* gateway/github: Add baseURL and uploadURL options

This allows using the gateway with github enterprise.

* Bump go-github to master HEAD, update locked deps

* Use webhook signature validation from go-github

* Use our gh.Hook instead of the response from API

The API, at least on Github Enterprise, strips the webhook secret from
the response, breaking the signature validation.

* Do not return error if webhook already exists

This is still pretty brittle. This should be redesigned in general, but
it makes it a big more robust for now.

gateways/community/github/config.go
@@ -56,6 +56,10 @@ type githubConfig struct {
 	Active bool `json:"active"`
 	// ContentType json or form
 	ContentType string `json:"contentType"`
+	// GitHub base URL (for GitHub Enterprise)
+	GithubBaseURL string `json:"githubBaseURL"`
+	// GitHub upload URL (for GitHub Enterprise)
+	GithubUploadURL string `json:"githubUploadURL"`
 }
 
 // cred stores the api access token or webhook secret

gateways/community/github/start.go
@@ -18,14 +18,9 @@ package github
 
 import (
 	"context"
-	"crypto/hmac"
-	"crypto/sha1"
-	"encoding/hex"
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strings"
+	"net/url"
 	"time"
 
 	"github.com/argoproj/argo-events/common"
@@ -107,17 +102,35 @@ func (ese *GithubEventSourceExecutor) PostActivate(rc *gwcommon.RouteConfig) err
 		Active: gh.Bool(gc.Active),
 		Config: hookConfig,
 	}
+	rc.Configs[labelWebhook] = hookSetup
 
 	client := gh.NewClient(PATTransport.Client())
+	if gc.GithubBaseURL != "" {
+		baseURL, err := url.Parse(gc.GithubBaseURL)
+		if err != nil {
+			return fmt.Errorf("failed to parse github base url. err: %s", err)
+		}
+		client.BaseURL = baseURL
+	}
+	if gc.GithubUploadURL != "" {
+		uploadURL, err := url.Parse(gc.GithubUploadURL)
+		if err != nil {
+			return fmt.Errorf("failed to parse github upload url. err: %s", err)
+		}
+		client.UploadURL = uploadURL
+	}
 	rc.Configs[labelGithubClient] = client
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	hook, _, err := client.Repositories.CreateHook(ctx, gc.Owner, gc.Repository, hookSetup)
 	if err != nil {
-		return fmt.Errorf("failed to create webhook. err: %+v", err)
+		// Continue if error is because hook already exists
+		er, ok := err.(*gh.ErrorResponse)
+		if !ok || er.Response.StatusCode != http.StatusUnprocessableEntity {
+			return fmt.Errorf("failed to create webhook. err: %+v", err)
+		}
 	}
-	rc.Configs[labelWebhook] = hook
 
 	ese.Log.Info().Str("event-source-name", rc.EventSource.Name).Interface("hook-id", *hook.ID).Msg("github hook created")
 	return nil
@@ -163,46 +176,6 @@ func (ese *GithubEventSourceExecutor) StartEventSource(eventSource *gateways.Eve
 	}, helper, eventStream)
 }
 
-func signBody(secret, body []byte) []byte {
-	computed := hmac.New(sha1.New, secret)
-	computed.Write(body)
-	return []byte(computed.Sum(nil))
-}
-
-func verifySignature(secret []byte, signature string, body []byte) bool {
-	const signaturePrefix = "sha1="
-	const signatureLength = 45
-
-	if len(signature) != signatureLength || !strings.HasPrefix(signature, signaturePrefix) {
-		return false
-	}
-
-	actual := make([]byte, 20)
-	hex.Decode(actual, []byte(signature[5:]))
-
-	return hmac.Equal(signBody(secret, body), actual)
-}
-
-func validatePayload(secret []byte, headers http.Header, body []byte) error {
-	signature := headers.Get(githubSignatureHeader)
-	if len(signature) == 0 {
-		return errors.New("no x-hub-signature header found")
-	}
-
-	if event := headers.Get(githubEventHeader); len(event) == 0 {
-		return errors.New("no x-github-event header found")
-	}
-
-	if id := headers.Get(githubDeliveryHeader); len(id) == 0 {
-		return errors.New("no x-github-delivery header found")
-	}
-
-	if !verifySignature(secret, signature, body) {
-		return errors.New("invalid signature")
-	}
-	return nil
-}
-
 // routeActiveHandler handles new route
 func RouteActiveHandler(writer http.ResponseWriter, request *http.Request, rc *gwcommon.RouteConfig) {
 	var response string
@@ -219,20 +192,16 @@ func RouteActiveHandler(writer http.ResponseWriter, request *http.Request, rc *g
 		return
 	}
 
-	body, err := ioutil.ReadAll(request.Body)
-	if err != nil {
-		logger.Error().Err(err).Msg("failed to parse request body")
-		common.SendErrorResponse(writer, fmt.Sprintf("failed to parse request. err: %+v", err))
-		return
-	}
-
 	hook := rc.Configs[labelWebhook].(*gh.Hook)
-	if secret, ok := hook.Config["secret"]; ok {
-		if err := validatePayload([]byte(secret.(string)), request.Header, body); err != nil {
-			logger.Error().Err(err).Msg("request is not valid event notification")
-			common.SendErrorResponse(writer, fmt.Sprintf("invalid event notification"))
-			return
-		}
+	secret := ""
+	if s, ok := hook.Config["secret"]; ok {
+		secret = s.(string)
+	}
+	body, err := gh.ValidatePayload(request, []byte(secret))
+	if err != nil {
+		logger.Error().Err(err).Msg("request is not valid event notification")
+		common.SendErrorResponse(writer, fmt.Sprintf("invalid event notification"))
+		return
 	}
 
 	helper.ActiveEndpoints[rc.Webhook.Endpoint].DataCh <- body

Gopkg.toml
@@ -128,7 +128,7 @@ required = [
 
 [[constraint]]
   name = "github.com/google/go-github"
-  version = "21.0.0"
+  revision = "50be09d24ee31a2b0868265e76c24b9545a6eb7a"
 
 [[constraint]]
   name = "k8s.io/gengo"

commit 0d62b353ad3d3f12517dd55772b15c5c973f5ab5

fix(): bug introduced from #221

examples/gateways/github-configmap.yaml
@@ -4,6 +4,7 @@ metadata:
   name: github-gateway-configmap
 data:
   project_1: |-
+    id: 1234
     owner: "owner-example"
     repository: "repo-example"
     hook:

examples/gateways/gitlab-configmap.yaml
@@ -4,6 +4,7 @@ metadata:
   name: gitlab-gateway-configmap
 data:
   project_1: |-
+    id: 1234
     projectId: "1"
     hook:
      endpoint: "/push"

gateways/common/webhook.go
@@ -227,23 +227,28 @@ func processChannels(rc RouteConfig, helper *WebhookHelper, eventStream gateways
 func ProcessRoute(rc RouteConfig, helper *WebhookHelper, eventStream gateways.Eventing_StartEventSourceServer) error {
 	r := rc.GetRoute()
 
+	r.Logger.Info().Str("event-source", r.EventSource.Name).Msg("validating the route")
 	if err := validateRoute(rc.GetRoute()); err != nil {
 		r.Logger.Error().Err(err).Str("event-source", r.EventSource.Name).Msg("error occurred validating route")
 		return err
 	}
 
+	r.Logger.Info().Str("event-source", r.EventSource.Name).Msg("activating the route")
 	activateRoute(rc, helper)
 
+	r.Logger.Info().Str("event-source", r.EventSource.Name).Msg("running post start")
 	if err := rc.PostStart(); err != nil {
 		r.Logger.Error().Err(err).Str("event-source", r.EventSource.Name).Msg("error occurred in post start")
 		return err
 	}
 
+	r.Logger.Info().Str("event-source", r.EventSource.Name).Msg("processing channels")
 	if err := processChannels(rc, helper, eventStream); err != nil {
 		r.Logger.Error().Err(err).Str("event-source", r.EventSource.Name).Msg("error occurred in process channel")
 		return err
 	}
 
+	r.Logger.Info().Str("event-source", r.EventSource.Name).Msg("running post stop")
 	if err := rc.PostStop(); err != nil {
 		r.Logger.Error().Err(err).Str("event-source", r.EventSource.Name).Msg("error occurred in post stop")
 	}

gateways/community/github/config.go
@@ -47,6 +47,8 @@ type RouteConfig struct {
 
 // githubEventSource contains information to setup a github project integration
 type githubEventSource struct {
+	// Webhook ID
+	Id int64 `json:"id"`
 	// Webhook
 	Hook *common.Webhook `json:"hook"`
 	// GitHub owner name i.e. argoproj

gateways/community/github/start.go
@@ -99,6 +99,7 @@ func (rc *RouteConfig) PostStart() error {
 		Events: gc.Events,
 		Active: gh.Bool(gc.Active),
 		Config: hookConfig,
+		ID:     &rc.ges.Id,
 	}
 
 	rc.client = gh.NewClient(PATTransport.Client())
@@ -128,7 +129,17 @@ func (rc *RouteConfig) PostStart() error {
 		}
 	}
 
-	rc.route.Logger.Info().Str("event-source-name", rc.route.EventSource.Name).Int64("hook-id", *hook.ID).Msg("github hook created")
+	if hook == nil {
+		ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		hook, _, err = rc.client.Repositories.GetHook(ctx, gc.Owner, gc.Repository, rc.ges.Id)
+		if err != nil {
+			return fmt.Errorf("failed to get existing webhook with id %d. err: %+v", rc.ges.Id, err)
+		}
+	}
+
+	rc.hook = hook
+	rc.route.Logger.Info().Str("event-source-name", rc.route.EventSource.Name).Msg("github hook created")
 	return nil
 }
 
@@ -139,7 +150,7 @@ func (rc *RouteConfig) PostStop() error {
 	if _, err := rc.client.Repositories.DeleteHook(ctx, rc.ges.Owner, rc.ges.Repository, *rc.hook.ID); err != nil {
 		return fmt.Errorf("failed to delete hook. err: %+v", err)
 	}
-	rc.route.Logger.Info().Str("event-source-name", rc.route.EventSource.Name).Int64("hook-id", *rc.hook.ID).Msg("github hook deleted")
+	rc.route.Logger.Info().Str("event-source-name", rc.route.EventSource.Name).Msg("github hook deleted")
 	return nil
 }
 
@@ -156,6 +167,12 @@ func (ese *GithubEventSourceExecutor) StartEventSource(eventSource *gateways.Eve
 	gc := config.(*githubEventSource)
 
 	return gwcommon.ProcessRoute(&RouteConfig{
+		route: &gwcommon.Route{
+			Logger:      &ese.Log,
+			EventSource: eventSource,
+			Webhook:     gc.Hook,
+			StartCh:     make(chan struct{}),
+		},
 		clientset: ese.Clientset,
 		namespace: ese.Namespace,
 		ges:       gc,

gateways/community/github/validate.go
@@ -30,6 +30,9 @@ func validateGithub(config interface{}) error {
 	if g == nil {
 		return gwcommon.ErrNilEventSource
 	}
+	if g.Id == 0 {
+		return fmt.Errorf("hook id must be not be zero")
+	}
 	if g.Repository == "" {
 		return fmt.Errorf("repository cannot be empty")
 	}

gateways/community/gitlab/config.go
@@ -46,6 +46,8 @@ type RouteConfig struct {
 
 // gitlabEventSource contains information to setup a gitlab project integration
 type gitlabEventSource struct {
+	// Webhook Id
+	Id int `json:"id"`
 	// Webhook
 	Hook *common.Webhook `json:"hook"`
 	// ProjectId is the id of project for which integration needs to setup

gateways/community/gitlab/start.go
@@ -66,8 +66,8 @@ func (rc *RouteConfig) PostStart() error {
 	formattedUrl := gwcommon.GenerateFormattedURL(rc.ges.Hook)
 
 	opt := &gitlab.AddProjectHookOptions{
-		URL:   &formattedUrl,
-		Token: &c.token,
+		URL:                   &formattedUrl,
+		Token:                 &c.token,
 		EnableSSLVerification: &rc.ges.EnableSSLVerification,
 	}
 
@@ -80,29 +80,16 @@ func (rc *RouteConfig) PostStart() error {
 	reflect.Indirect(iev).SetBool(true)
 	elem.Set(iev)
 
-	existingHooks, _, err := rc.client.Projects.ListProjectHooks(rc.ges.ProjectId, &gitlab.ListProjectHooksOptions{})
+	hook, _, err := rc.client.Projects.GetProjectHook(rc.ges.ProjectId, rc.ges.Id)
 	if err != nil {
-		return fmt.Errorf("failed to list existing webhooks. err: %+v", err)
-	}
-
-	for _, h := range existingHooks {
-		elem = reflect.ValueOf(h).Elem().FieldByName(string(rc.ges.Event))
-		if ok := elem.IsValid(); !ok {
-			return fmt.Errorf("failed to check if hook is already registered with the event %s. unknown event", rc.ges.Event)
-		}
-		if reflect.New(elem.Type().Elem()).Bool() && h.URL == formattedUrl {
-			rc.route.Logger.Warn().Str("event-source", rc.route.EventSource.Name).Msg("hook is already configured")
-			return nil
+		hook, _, err = rc.client.Projects.AddProjectHook(rc.ges.ProjectId, opt)
+		if err != nil {
+			return fmt.Errorf("failed to add project hook. err: %+v", err)
 		}
 	}
 
-	hook, _, err := rc.client.Projects.AddProjectHook(rc.ges.ProjectId, opt)
-	if err != nil {
-		return fmt.Errorf("failed to add project hook. err: %+v", err)
-	}
 	rc.hook = hook
-
-	rc.route.Logger.Info().Str("event-source-name", rc.route.EventSource.Name).Int("hook-id", rc.hook.ID).Msg("gitlab hook created")
+	rc.route.Logger.Info().Str("event-source-name", rc.route.EventSource.Name).Msg("gitlab hook created")
 	return nil
 }
 
@@ -110,7 +97,7 @@ func (rc *RouteConfig) PostStop() error {
 	if _, err := rc.client.Projects.DeleteProjectHook(rc.ges.ProjectId, rc.hook.ID); err != nil {
 		return fmt.Errorf("failed to delete hook. err: %+v", err)
 	}
-	rc.route.Logger.Info().Str("event-source-name", rc.route.EventSource.Name).Int("hook-id", rc.hook.ID).Msg("gitlab hook deleted")
+	rc.route.Logger.Info().Str("event-source-name", rc.route.EventSource.Name).Msg("gitlab hook deleted")
 	return nil
 }
 

gateways/community/gitlab/validate.go
@@ -31,6 +31,9 @@ func validateGitlab(config interface{}) error {
 	if g == nil {
 		return gwcommon.ErrNilEventSource
 	}
+	if g.Id == 0 {
+		return fmt.Errorf("hook id must be not be zero")
+	}
 	if g.ProjectId == "" {
 		return fmt.Errorf("project id can't be empty")
 	}

gateways/gateway.go
@@ -18,12 +18,12 @@ package gateways
 
 import (
 	"fmt"
-	"net"
-	"os"
-
 	"github.com/argoproj/argo-events/common"
 	zlog "github.com/rs/zerolog"
 	"google.golang.org/grpc"
+	"net"
+	"os"
+	"runtime/debug"
 )
 
 // StartGateway start a gateway
@@ -50,6 +50,7 @@ func StartGateway(es EventingServer) {
 func Recover(eventSource string) {
 	if r := recover(); r != nil {
 		fmt.Printf("recovered event source %s from error. recover: %v", eventSource, r)
+		debug.PrintStack()
 	}
 }
 

gateways/community/github/config_test.go
@@ -22,6 +22,7 @@ import (
 )
 
 var es = `
+id: 1234
 hook:
  endpoint: "/push"
  port: "12000"

gateways/community/github/validate_test.go
@@ -11,6 +11,7 @@ var (
 	configKey  = "testConfig"
 	configId   = "1234"
 	goodConfig = `
+id: 1234
 hook:
  endpoint: "/push"
  port: "12000"

gateways/community/gitlab/config_test.go
@@ -23,6 +23,7 @@ import (
 )
 
 var es = `
+id: 12
 hook:
  endpoint: "/push"
  port: "12000"

gateways/community/gitlab/validate_test.go
@@ -27,6 +27,7 @@ var (
 	configKey   = "testConfig"
 	configId    = "1234"
 	validConfig = `
+id: 1234
 hook:
  endpoint: "/push"
  port: "12000"