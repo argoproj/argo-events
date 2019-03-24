40791c21e968bce66894c8c003dbb5209ca62d20
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2019-03-24 16:20:25 +0900

    Update e2e tests

--- test/e2e/common/client.go
+++ test/e2e/common/client.go
@@ -1,12 +1,19 @@
 package common
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
+
+	"github.com/pkg/errors"
 
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
@@ -18,7 +25,10 @@ import (
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes/scheme"
+	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
 
 	gwv1 "github.com/argoproj/argo-events/pkg/apis/gateway/v1alpha1"
 	sv1 "github.com/argoproj/argo-events/pkg/apis/sensor/v1alpha1"
@@ -41,6 +51,7 @@ func init() {
 }
 
 type E2EClient struct {
+	Config *restclient.Config
 	dynamic.ClientPool
 	discovery.DiscoveryClient
 	E2EID    string
@@ -62,6 +73,7 @@ func NewE2EClient(e2eID string) (*E2EClient, error) {
 	clientID := strconv.FormatUint(rand.Uint64(), 16)
 
 	return &E2EClient{
+		Config:          config,
 		ClientPool:      clientPool,
 		DiscoveryClient: *disco,
 		E2EID:           e2eID,
@@ -108,7 +120,7 @@ func (clpl *E2EClient) CreateTmpNamespace() (string, error) {
 		RoleRef: rbacv1.RoleRef{
 			APIGroup: "rbac.authorization.k8s.io",
 			Kind:     "ClusterRole",
-			Name:     "edit",
+			Name:     "argo-events-role",
 		},
 	}
 
@@ -125,8 +137,24 @@ func (clpl *E2EClient) DeleteNamespaces() error {
 	if err != nil {
 		return err
 	}
+	// DeleteCollection is not supported for namespaces,
+	// so list namespaces and delete them one by one.
 	labelSelector := labels.Set(map[string]string{ArgoEventsE2ETestClientIDLabelKey: clpl.ClientID}).String()
-	return client.DeleteCollection(&metav1.DeleteOptions{}, metav1.ListOptions{LabelSelector: labelSelector})
+	obj, err := client.List(metav1.ListOptions{LabelSelector: labelSelector})
+	if err != nil {
+		return err
+	}
+	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return err
+	}
+	for _, ns := range (unstructured.UnstructuredList{Object: uObj}).Items {
+		err := client.Delete(ns.GetName(), &metav1.DeleteOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (clpl *E2EClient) GetClient(namespace string, gvk schema.GroupVersionKind) (dynamic.ResourceInterface, error) {
@@ -162,4 +190,124 @@ func (clpl *E2EClient) Create(namespace string, obj runtime.Object) (*unstructur
 	}
 
 	return client.Create(unstructuredObj)
+}
+
+func (clpl *E2EClient) CreateResourceFromYaml(namespace, path string, modFunc func(*unstructured.Unstructured) error) (*unstructured.Unstructured, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	obj, _, err := scheme.Codecs.UniversalDeserializer().Decode(bytes, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	unstructuredObj := &unstructured.Unstructured{Object: uObj}
+
+	if modFunc != nil {
+		err = modFunc(unstructuredObj)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return clpl.Create(namespace, unstructuredObj)
+}
+
+func (clpl *E2EClient) Get(namespace string, gvk schema.GroupVersionKind, name string) (*unstructured.Unstructured, error) {
+	client, err := clpl.GetClient(namespace, gvk)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.Get(name, metav1.GetOptions{})
+}
+
+func (clpl *E2EClient) GetPod(namespace string, name string) (*corev1.Pod, error) {
+	client, err := clpl.GetClient(namespace, schema.GroupVersionKind{Version: "v1", Kind: "Pod"})
+	if err != nil {
+		return nil, err
+	}
+
+	uObj, err := client.Get(name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	newObj := &corev1.Pod{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(uObj.Object, newObj)
+	if err != nil {
+		return nil, err
+	}
+	return newObj, nil
+}
+
+func (clpl *E2EClient) GetService(namespace string, name string) (*corev1.Service, error) {
+	client, err := clpl.GetClient(namespace, schema.GroupVersionKind{Version: "v1", Kind: "Service"})
+	if err != nil {
+		return nil, err
+	}
+
+	uObj, err := client.Get(name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	newObj := &corev1.Service{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(uObj.Object, newObj)
+	if err != nil {
+		return nil, err
+	}
+	return newObj, nil
+}
+
+func (clpl *E2EClient) ForwardServicePort(tmpNamespace, podName string, localPort, targetPort int) (chan struct{}, error) {
+	// Implementation ref: https://github.com/kubernetes/client-go/issues/51#issuecomment-436200428
+	roundTripper, upgrader, err := spdy.RoundTripperFor(clpl.Config)
+	if err != nil {
+		return nil, err
+	}
+
+	path := fmt.Sprintf("/api/v1/namespaces/%s/pods/%s/portforward", tmpNamespace, podName)
+	hostIP := strings.TrimLeft(clpl.Config.Host, "https://")
+	serverURL := url.URL{Scheme: "https", Path: path, Host: hostIP}
+
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: roundTripper}, http.MethodPost, &serverURL)
+
+	stopChan, readyChan := make(chan struct{}, 1), make(chan struct{}, 1)
+
+	portDesc := fmt.Sprintf("%d:%d", localPort, targetPort)
+	out, errOut := new(bytes.Buffer), new(bytes.Buffer)
+	forwarder, err := portforward.New(dialer, []string{portDesc}, stopChan, readyChan, out, errOut)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		err = forwarder.ForwardPorts()
+		if err != nil {
+			fmt.Printf("%+v\n", err)
+		}
+	}()
+
+	err = nil
+L:
+	for {
+		select {
+		case <-time.After(10 * time.Second):
+			err = errors.New("timed out port forwarding")
+			break L
+		case <-readyChan:
+			break L
+		default:
+		}
+	}
+
+	return stopChan, err
 }
--- test/e2e/core/main_test.go
+++ test/e2e/core/main_test.go
@@ -2,19 +2,22 @@ package core
 
 import (
 	"fmt"
+	"net"
+	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
+	"time"
 
-	/*
+	"github.com/smartystreets/goconvey/convey"
 
-	   "net/http"
-	   "net/url"
-	*/
-
-	common "github.com/argoproj/argo-events/common"
+	"github.com/argoproj/argo-events/common"
 	e2ecommon "github.com/argoproj/argo-events/test/e2e/common"
+	corev1 "k8s.io/api/core/v1"
+	apierr "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 var (
@@ -35,33 +38,28 @@ func setup() error {
 		return err
 	}
 	tmpNamespace = namespace
-	fmt.Printf("tmpNamespace: %v\n", tmpNamespace)
 	return nil
 }
 
-func teardown() error {
+func teardown() {
 	if !e2ecommon.KeepNamespace() {
-		err := client.DeleteNamespaces()
-		if err != nil {
-			return err
+		if client != nil {
+			err := client.DeleteNamespaces()
+			if err != nil {
+				fmt.Printf("%+v\n", err)
+			}
 		}
 	}
-	return nil
 }
 
 func TestMain(m *testing.M) {
 	err := setup()
 	if err != nil {
+		teardown()
 		panic(err)
 	}
-
 	ret := m.Run()
-
-	err = teardown()
-	if err != nil {
-		fmt.Errorf("%+v\n", err)
-	}
-
+	teardown()
 	os.Exit(ret)
 }
 
@@ -73,46 +71,135 @@ func TestGeneralUseCase(t *testing.T) {
 	}
 	manifestsDir := filepath.Join(dir, "manifests")
 
-	gateway, err := e2ecommon.ReadResource(filepath.Join(manifestsDir, "webhook-gateway.yaml"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	e2ecommon.SetLabel(gateway, common.LabelKeyGatewayControllerInstanceID, e2eID)
+	convey.Convey("Test the general use case", t, func() {
+		convey.Convey("Create a gateway.", func() {
+			_, err := client.CreateResourceFromYaml(tmpNamespace, filepath.Join(manifestsDir, "webhook-gateway.yaml"), func(obj *unstructured.Unstructured) error {
+				e2ecommon.SetLabel(obj, common.LabelKeyGatewayControllerInstanceID, e2eID)
+				return nil
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+			_, err = client.CreateResourceFromYaml(tmpNamespace, filepath.Join(manifestsDir, "webhook-gateway-configmap.yaml"), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
 
-	cm, err := e2ecommon.ReadResource(filepath.Join(manifestsDir, "webhook-gateway-configmap.yaml"))
-	if err != nil {
-		t.Fatal(err)
-	}
+		convey.Convey("Create a sensor.", func() {
+			_, err := client.CreateResourceFromYaml(tmpNamespace, filepath.Join(manifestsDir, "webhook-sensor.yaml"), func(obj *unstructured.Unstructured) error {
+				e2ecommon.SetLabel(obj, common.LabelKeySensorControllerInstanceID, e2eID)
+				return nil
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
 
-	sensor, err := e2ecommon.ReadResource(filepath.Join(manifestsDir, "webhook-sensor.yaml"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	e2ecommon.SetLabel(sensor, common.LabelKeySensorControllerInstanceID, e2eID)
+		convey.Convey("Wait for corresponding resources.", func() {
+			ticker := time.NewTicker(time.Second)
+			defer ticker.Stop()
+			var gwpod, spod *corev1.Pod
+			var gwsvc *corev1.Service
+		L:
+			for {
+				select {
+				case _ = <-ticker.C:
+					if gwpod == nil {
+						pod, err := client.GetPod(tmpNamespace, "webhook-gateway")
+						if err != nil && !apierr.IsNotFound(err) {
+							t.Error(err)
+							break L
+						}
+						if pod != nil && pod.Status.Phase == corev1.PodRunning {
+							gwpod = pod
+						}
+					}
+					if gwsvc == nil {
+						svc, err := client.GetService(tmpNamespace, "webhook-gateway-svc")
+						if err != nil && !apierr.IsNotFound(err) {
+							t.Error(err)
+							break L
+						}
+						gwsvc = svc
+					}
+					if spod == nil {
+						pod, err := client.GetPod(tmpNamespace, "webhook-sensor")
+						if err != nil && !apierr.IsNotFound(err) {
+							t.Error(err)
+							break L
+						}
+						if pod != nil && pod.Status.Phase == corev1.PodRunning {
+							spod = pod
+						}
+					}
+					if gwpod != nil && gwsvc != nil && spod != nil {
+						break L
+					}
+				case <-time.After(10 * time.Second):
+					t.Error("timed out gateway and sensor startup")
+					break L
+				}
+			}
+			if t.Failed() {
+				t.FailNow()
+			}
+		})
 
-	_, err = client.Create(tmpNamespace, gateway)
-	if err != nil {
-		t.Fatal(err)
-	}
+		convey.Convey("Make a request to the gateway.", func() {
+			// Avoid too early access
+			time.Sleep(1 * time.Second)
 
-	_, err = client.Create(tmpNamespace, cm)
-	if err != nil {
-		t.Fatal(err)
-	}
+			// Use available port
+			l, _ := net.Listen("tcp", ":0")
+			port := l.Addr().(*net.TCPAddr).Port
+			l.Close()
 
-	_, err = client.Create(tmpNamespace, sensor)
-	if err != nil {
-		t.Fatal(err)
-	}
-	/*
+			// Use port forwarding to access pods in minikube
+			stopChan, err := client.ForwardServicePort(tmpNamespace, "webhook-gateway", port, 12000)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer close(stopChan)
 
-	   req, err := http.NewRequest(method, endpointURL.String(), body)
-	   if err != nil {
-	       return nil, err
-	   }
+			url := fmt.Sprintf("http://localhost:%d/foo", port)
+			req, err := http.NewRequest("POST", url, strings.NewReader("e2e"))
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	   req = req.WithContext(ctx)
+			resp, err := new(http.Client).Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer resp.Body.Close()
 
-	*/
+			if t.Failed() {
+				t.FailNow()
+			}
+		})
 
+		convey.Convey("Check if the sensor trigggered a pod.", func() {
+			ticker2 := time.NewTicker(time.Second)
+			defer ticker2.Stop()
+		L:
+			for {
+				select {
+				case _ = <-ticker2.C:
+					pod, err := client.GetPod(tmpNamespace, "webhook-sensor-triggered-pod")
+					if err != nil && !apierr.IsNotFound(err) {
+						t.Error(err)
+						break L
+					}
+					if pod != nil && pod.Status.Phase == corev1.PodSucceeded {
+						convey.So(pod.Spec.Containers[0].Args[0], convey.ShouldEqual, "e2e")
+						break L
+					}
+				case <-time.After(10 * time.Second):
+					t.Error("timed out gateway and sensor startup")
+					break L
+				}
+			}
+		})
+	})
 }
