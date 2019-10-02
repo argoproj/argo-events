9605b8b6eeefbc7143c756bcd4daf945d7593e8a
Justin Taylor-Barrick <46463088+jbarrick-mesosphere@users.noreply.github.com>
2019-10-02 07:29:55 -0700

    Support adding Github hooks without requiring a Github webhook id to be hardcoded (#352)

---
+++ gateways/community/github/hook_util.go
@@ -0,0 +1,52 @@
+package github
+
+import (
+	gh "github.com/google/go-github/github"
+)
+
+// sliceEqual returns true if the two provided string slices are equal.
+func sliceEqual(first []string, second []string) bool {
+	if len(first) != len(second) {
+		return false
+	}
+
+	if first == nil && second == nil {
+		return true
+	}
+
+	if first == nil || second == nil {
+		return false
+	}
+
+	for index, _ := range first {
+		if first[index] != second[index] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// compareHook returns true if the hook matches the url and event.
+func compareHook(hook *gh.Hook, url string, event []string) bool {
+	if hook == nil {
+		return false
+	}
+
+	if hook.Config["url"] != url {
+		return false
+	}
+
+	return sliceEqual(hook.Events, event)
+}
+
+// getHook returns the hook that matches the url and event, or nil if not found.
+func getHook(hooks []*gh.Hook, url string, event []string) *gh.Hook {
+	for _, hook := range hooks {
+		if compareHook(hook, url, event) {
+			return hook
+		}
+	}
+
+	return nil
+}
