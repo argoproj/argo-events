59cc8019be172c652501ee8c8f8d06704fba5c5a
VaibhavPage <vaibhav.page@gmail.com>
2020-02-19 15:39:03 -0500

    fix(resource-gateway): fix test

--- gateways/server/resource/start_test.go
+++ gateways/server/resource/start_test.go
@@ -17,7 +17,6 @@ limitations under the License.
 package resource
 
 import (
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"testing"
 
 	"github.com/argoproj/argo-events/pkg/apis/eventsources/v1alpha1"
@@ -25,6 +24,7 @@ import (
 	"github.com/smartystreets/goconvey/convey"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/kubernetes/fake"
 )
 
