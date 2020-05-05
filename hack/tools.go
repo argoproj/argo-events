144d3e45dd0b0982ae67cacc2c59322276cd85e3
Derek Wang <whynowy@gmail.com>
2020-05-04 22:14:41 -0700

    tools chanqge

--- hack/tools.go
+++ hack/tools.go
@@ -1,9 +1,12 @@
+// +build tools
+
 // This package contains code generation utilities
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
 
 import (
 	_ "github.com/ahmetb/gen-crd-api-reference-docs"
+	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/conversion-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
