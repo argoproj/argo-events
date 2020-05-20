10e145e443bf3438892370b43247ad63245a8fd0
Derek Wang <whynowy@gmail.com>
2020-05-20 10:09:00 -0700

    chore: Improve the performance of make codegen (#665)
    * chore: Improve the performance of make codegen
    
    * comments
    
    * enhance
    
    * minor
    
    * typo
    
    * refac

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,36 +4,39 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Expected performance drop after moving to go mod
-# see https://github.com/kubernetes/code-generator/issues/69
-# TODO: fix it
+source $(dirname $0)/library.sh
 
-serial=`date "+%Y%m%d%H%M%S"`
-output_dir="/tmp/${serial}"
-mkdir -p ${output_dir}
+if [ ! -d "${REPO_ROOT}/vendor" ]; then
+  go mod vendor
+fi
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+make_fake_paths
+
+export GOPATH="${FAKE_GOPATH}"
+export GO111MODULE="off"
+
+cd "${FAKE_REPOPATH}"
+
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${FAKE_REPOPATH}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
 bash -x ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/argoproj/argo-events/pkg/client/sensor github.com/argoproj/argo-events/pkg/apis \
   "sensor:v1alpha1" \
-  --go-header-file $SCRIPT_ROOT/hack/custom-boilerplate.go.txt --output-base ${output_dir}
+  --go-header-file hack/custom-boilerplate.go.txt
 
 bash -x ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/argoproj/argo-events/pkg/client/gateway github.com/argoproj/argo-events/pkg/apis \
   "gateway:v1alpha1" \
-  --go-header-file $SCRIPT_ROOT/hack/custom-boilerplate.go.txt --output-base ${output_dir}
+  --go-header-file hack/custom-boilerplate.go.txt
 
 bash -x ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/argoproj/argo-events/pkg/client/eventsources github.com/argoproj/argo-events/pkg/apis \
   "eventsources:v1alpha1" \
-  --go-header-file $SCRIPT_ROOT/hack/custom-boilerplate.go.txt --output-base ${output_dir}
+  --go-header-file hack/custom-boilerplate.go.txt
 
-go run $SCRIPT_ROOT/vendor/k8s.io/gengo/examples/deepcopy-gen/main.go -i github.com/argoproj/argo-events/pkg/apis/common -p github.com/argoproj/argo-events/pkg/apis/common \
-   --output-base ${output_dir} \
-   --go-header-file $SCRIPT_ROOT/hack/custom-boilerplate.go.txt
+go run $FAKE_REPOPATH/vendor/k8s.io/gengo/examples/deepcopy-gen/main.go -i github.com/argoproj/argo-events/pkg/apis/common -p github.com/argoproj/argo-events/pkg/apis/common \
+   --go-header-file hack/custom-boilerplate.go.txt
 
-cp -r ${output_dir}/github.com/argoproj/argo-events/* ${SCRIPT_ROOT}/
-rm -rf ${output_dir}
+# gofmt the tree
+find . -name "*.go" -type f -print0 | xargs -0 gofmt -s -w
 
