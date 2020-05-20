ac90267f987ed901857242bf49c48033482df16d
Derek Wang <whynowy@gmail.com>
2020-05-20 10:09:00 -0700

    chore: Improve the performance of make codegen (#665)
    * chore: Improve the performance of make codegen
    
    * comments
    
    * enhance
    
    * minor
    
    * typo
    
    * refac

--- hack/update-api-docs.sh
+++ hack/update-api-docs.sh
@@ -6,31 +6,45 @@ set -o pipefail
 
 # Setup at https://github.com/ahmetb/gen-crd-api-reference-docs
 
-readonly SCRIPT_ROOT="$(git rev-parse --show-toplevel)"
+source $(dirname $0)/library.sh
+
+ensure_pandoc
+
+if [ ! -d "${REPO_ROOT}/vendor" ]; then
+  go mod vendor
+fi
+
+make_fake_paths
+
+export GOPATH="${FAKE_GOPATH}"
+export GO111MODULE="off"
+
+cd "${FAKE_REPOPATH}"
 
 # Event Source
-go run ${SCRIPT_ROOT}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/main.go \
- -config "${SCRIPT_ROOT}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/example-config.json" \
+go run ${FAKE_REPOPATH}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/main.go \
+ -config "${FAKE_REPOPATH}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/example-config.json" \
  -api-dir "github.com/argoproj/argo-events/pkg/apis/eventsources/v1alpha1" \
- -out-file "${SCRIPT_ROOT}/api/event-source.html" \
- -template-dir "${SCRIPT_ROOT}/hack/api-docs-template"
+ -out-file "${FAKE_REPOPATH}/api/event-source.html" \
+ -template-dir "${FAKE_REPOPATH}/hack/api-docs-template"
 
 # Gateway
-go run ${SCRIPT_ROOT}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/main.go \
- -config "${SCRIPT_ROOT}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/example-config.json" \
+go run ${FAKE_REPOPATH}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/main.go \
+ -config "${FAKE_REPOPATH}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/example-config.json" \
  -api-dir "github.com/argoproj/argo-events/pkg/apis/gateway/v1alpha1" \
- -out-file "${SCRIPT_ROOT}/api/gateway.html" \
- -template-dir "${SCRIPT_ROOT}/hack/api-docs-template"
+ -out-file "${FAKE_REPOPATH}/api/gateway.html" \
+ -template-dir "${FAKE_REPOPATH}/hack/api-docs-template"
 
 # Sensor
-go run ${SCRIPT_ROOT}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/main.go \
- -config "${SCRIPT_ROOT}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/example-config.json" \
+go run ${FAKE_REPOPATH}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/main.go \
+ -config "${FAKE_REPOPATH}/vendor/github.com/ahmetb/gen-crd-api-reference-docs/example-config.json" \
  -api-dir "github.com/argoproj/argo-events/pkg/apis/sensor/v1alpha1" \
- -out-file "${SCRIPT_ROOT}/api/sensor.html" \
- -template-dir "${SCRIPT_ROOT}/hack/api-docs-template"
+ -out-file "${FAKE_REPOPATH}/api/sensor.html" \
+ -template-dir "${FAKE_REPOPATH}/hack/api-docs-template"
 
 # Setup at https://pandoc.org/installing.html
 
-pandoc --from markdown --to gfm ${SCRIPT_ROOT}/api/event-source.html > ${SCRIPT_ROOT}/api/event-source.md
-pandoc --from markdown --to gfm ${SCRIPT_ROOT}/api/gateway.html > ${SCRIPT_ROOT}/api/gateway.md
-pandoc --from markdown --to gfm ${SCRIPT_ROOT}/api/sensor.html > ${SCRIPT_ROOT}/api/sensor.md
+pandoc --from markdown --to gfm ${FAKE_REPOPATH}/api/event-source.html > ${FAKE_REPOPATH}/api/event-source.md
+pandoc --from markdown --to gfm ${FAKE_REPOPATH}/api/gateway.html > ${FAKE_REPOPATH}/api/gateway.md
+pandoc --from markdown --to gfm ${FAKE_REPOPATH}/api/sensor.html > ${FAKE_REPOPATH}/api/sensor.md
+
--- hack/update-openapigen.sh
+++ hack/update-openapigen.sh
@@ -4,38 +4,40 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-PROJECT_ROOT="$(git rev-parse --show-toplevel)"
-CODEGEN_PKG=${PROJECT_ROOT}/vendor/k8s.io/kube-openapi
+source $(dirname $0)/library.sh
+
+if [ ! -d "${REPO_ROOT}/vendor" ]; then
+  go mod vendor
+fi
+
+make_fake_paths
+
+export GOPATH="${FAKE_GOPATH}"
+export GO111MODULE="off"
+
+CODEGEN_PKG=${FAKE_REPOPATH}/vendor/k8s.io/kube-openapi
 VERSION="v1alpha1"
 
-serial=`date "+%Y%m%d%H%M%S"`
-output_dir="/tmp/${serial}"
-mkdir -p ${output_dir}G
+cd "${FAKE_REPOPATH}"
 
 # Sensor
 go run ${CODEGEN_PKG}/cmd/openapi-gen/openapi-gen.go \
-    --go-header-file ${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
+    --go-header-file ${REPO_ROOT}/hack/custom-boilerplate.go.txt \
     --input-dirs github.com/argoproj/argo-events/pkg/apis/sensor/${VERSION} \
     --output-package github.com/argoproj/argo-events/pkg/apis/sensor/${VERSION} \
-    --output-base ${output_dir} \
     $@
 
 # Gateway
 go run ${CODEGEN_PKG}/cmd/openapi-gen/openapi-gen.go \
-    --go-header-file ${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
+    --go-header-file ${REPO_ROOT}/hack/custom-boilerplate.go.txt \
     --input-dirs github.com/argoproj/argo-events/pkg/apis/gateway/${VERSION} \
     --output-package github.com/argoproj/argo-events/pkg/apis/gateway/${VERSION} \
-    --output-base ${output_dir} \
     $@
 
 # EventSource
 go run ${CODEGEN_PKG}/cmd/openapi-gen/openapi-gen.go \
-    --go-header-file ${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
+    --go-header-file ${REPO_ROOT}/hack/custom-boilerplate.go.txt \
     --input-dirs github.com/argoproj/argo-events/pkg/apis/eventsources/${VERSION} \
     --output-package github.com/argoproj/argo-events/pkg/apis/eventsources/${VERSION} \
-    --output-base ${output_dir} \
     $@
-
-cp -r ${output_dir}/github.com/argoproj/argo-events/* ${PROJECT_ROOT}/
-rm -rf ${output_dir}
 
