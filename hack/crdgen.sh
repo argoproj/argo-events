a0bb836a6e2cb3069c837c3651071839d349cd2f
Alex Collins <alex_collins@intuit.com>
2020-06-16 12:00:12 -0700

    feat-proto

--- hack/crdgen.sh
+++ hack/crdgen.sh
@@ -3,10 +3,6 @@ set -eu -o pipefail
 
 cd "$(dirname "$0")/.."
 
-del() {
-  yq delete $1 $2 >tmp
-  mv tmp "$1"
-}
 
 add_header() {
   cat "$1" | ./hack/auto-gen-msg.sh >tmp
@@ -17,19 +13,12 @@ if [ "$(command -v controller-gen)" = "" ]; then
   go install sigs.k8s.io/controller-tools/cmd/controller-gen
 fi
 
-if [ "$(command -v yq)" = "" ]; then
-  brew install yq
-fi
-
 echo "Generating CRDs"
 controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds
 
 find manifests/base/crds -name 'argoproj.io*.yaml' | while read -r file; do
   echo "Patching ${file}"
   # remove junk fields
-  del "$file" metadata.annotations
-  del "$file" metadata.creationTimestamp
-  del "$file" spec.validation
-  del "$file" status
+  go run ./hack cleancrd "$file"
   add_header "$file"
 done
