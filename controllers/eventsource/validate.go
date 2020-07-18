adeb8811ad5e40c8bf62b397833a47239b467882
Derek Wang <whynowy@gmail.com>
2020-07-17 22:37:08 -0700

    avoid duplicated event name in one eventsource

--- controllers/eventsource/validate.go
+++ controllers/eventsource/validate.go
@@ -13,28 +13,33 @@ import (
 
 // ValidateEventSource validates if the eventSource is valid
 func ValidateEventSource(eventSource *v1alpha1.EventSource) error {
-	servers := eventsources.GetEventingServers(eventSource)
 	recreateTypes := make(map[apicommon.EventSourceType]bool)
 	for _, esType := range apicommon.RecreateStrategyEventSources {
 		recreateTypes[esType] = true
 	}
+
+	servers := eventsources.GetEventingServers(eventSource)
+
+	eventNames := make(map[string]bool)
 	rollingUpdates, recreates := 0, 0
-	for eventType := range servers {
+	ctx := context.Background()
+	for eventType, ss := range servers {
 		if _, ok := recreateTypes[eventType]; ok {
 			recreates++
 		} else {
 			rollingUpdates++
 		}
-	}
-	if rollingUpdates > 0 && recreates > 0 {
-		// We don't allow this as if we use recreate strategy for the deployment it will have downtime
-		eventSource.Status.MarkSourcesNotProvided("InvalidEventSource", "Some types of event sources can not be put in one spec")
-		return errors.New("eventsources in the spec require both rolling update and recreate update strategy")
-	}
 
-	ctx := context.Background()
-	for _, ss := range servers {
 		for _, server := range ss {
+			eName := server.GetEventName()
+			if _, ok := eventNames[eName]; !ok {
+				eventNames[eName] = true
+			} else {
+				// Duplicated event name not allowed in one EventSource, even they are in different EventSourceType.
+				eventSource.Status.MarkSourcesNotProvided("InvalidEventSource", fmt.Sprintf("more than one \"%s\" found", eName))
+				return errors.Errorf("more than one \"%s\" found in the spec", eName)
+			}
+
 			err := server.ValidateEventSource(ctx)
 			if err != nil {
 				eventSource.Status.MarkSourcesNotProvided("InvalidEventSource", fmt.Sprintf("Invalid spec: %s - %s", server.GetEventSourceName(), server.GetEventName()))
@@ -42,6 +47,13 @@ func ValidateEventSource(eventSource *v1alpha1.EventSource) error {
 			}
 		}
 	}
+
+	if rollingUpdates > 0 && recreates > 0 {
+		// We don't allow this as if we use recreate strategy for the deployment it will have downtime
+		eventSource.Status.MarkSourcesNotProvided("InvalidEventSource", "Some types of event sources can not be put in one spec")
+		return errors.New("eventsources in the spec require both rolling update and recreate update strategy")
+	}
+
 	eventSource.Status.MarkSourcesProvided()
 	return nil
 }
