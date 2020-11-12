f66b67a2b19251827164d41117cf4a8859f13cde
Alex Collins <alex_collins@intuit.com>
2020-11-12 11:11:57 -0800

    logi:  M pkg/apis/sensor/v1alpha1/types_test.go
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- pkg/apis/sensor/v1alpha1/types_test.go
+++ pkg/apis/sensor/v1alpha1/types_test.go
@@ -9,5 +9,5 @@ import (
 func TestEvent_DataString(t *testing.T) {
 	assert.Empty(t, Event{}.DataString())
 	assert.Equal(t, "bXktZGF0YQ==", Event{Data: []byte("my-data")}.DataString())
-	assert.Equal(t, `"my-data"`, Event{Data: []byte(`"my-data"`), Context: &EventContext{DataContentType: "application/json",}}.DataString())
+	assert.Equal(t, `"my-data"`, Event{Data: []byte(`"my-data"`), Context: &EventContext{DataContentType: "application/json"}}.DataString())
 }
