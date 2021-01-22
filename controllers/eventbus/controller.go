74b9fc242f28b024846d3f1e4b8289fbd5bb4c43
Derek Wang <whynowy@gmail.com>
2021-01-22 12:32:26 -0800

    feat: use status resources for controller reconciliation. Closes #1029 (#1030)
    * feat: use status resources for controller reconciliation
    
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- controllers/eventbus/controller.go
+++ controllers/eventbus/controller.go
@@ -7,9 +7,9 @@ import (
 	"k8s.io/apimachinery/pkg/api/equality"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/sets"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/argoproj/argo-events/controllers/eventbus/installer"
@@ -44,19 +44,22 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 			r.logger.Warnw("WARNING: eventbus not found", "request", req)
 			return reconcile.Result{}, nil
 		}
-		r.logger.Errorw("unable to get eventbus ctl", "request", req, "error", err)
+		r.logger.Errorw("unable to get eventbus ctl", zap.Any("request", req), zap.Error(err))
 		return ctrl.Result{}, err
 	}
 	log := r.logger.With("namespace", eventBus.Namespace).With("eventbus", eventBus.Name)
 	busCopy := eventBus.DeepCopy()
 	reconcileErr := r.reconcile(ctx, busCopy)
 	if reconcileErr != nil {
-		log.Desugar().Error("reconcile error", zap.Error(reconcileErr))
+		log.Errorw("reconcile error", zap.Error(reconcileErr))
 	}
 	if r.needsUpdate(eventBus, busCopy) {
 		if err := r.client.Update(ctx, busCopy); err != nil {
 			return reconcile.Result{}, err
 		}
+	}
+	if err := r.client.Status().Update(ctx, busCopy); err != nil {
+		return reconcile.Result{}, err
 	}
 	return ctrl.Result{}, reconcileErr
 }
@@ -66,38 +69,24 @@ func (r *reconciler) reconcile(ctx context.Context, eventBus *v1alpha1.EventBus)
 	log := r.logger.With("namespace", eventBus.Namespace).With("eventbus", eventBus.Name)
 	if !eventBus.DeletionTimestamp.IsZero() {
 		log.Info("deleting eventbus")
-		// Finalizer logic should be added here.
-		err := installer.Uninstall(eventBus, r.client, r.natsStreamingImage, r.natsMetricsImage, log)
-		if err != nil {
-			log.Errorw("failed to uninstall", "error", err)
-			return nil
+		if controllerutil.ContainsFinalizer(eventBus, finalizerName) {
+			// Finalizer logic should be added here.
+			if err := installer.Uninstall(eventBus, r.client, r.natsStreamingImage, r.natsMetricsImage, log); err != nil {
+				log.Errorw("failed to uninstall", zap.Error(err))
+				return nil
+			}
+			controllerutil.RemoveFinalizer(eventBus, finalizerName)
 		}
-		r.removeFinalizer(eventBus)
 		return nil
 	}
-	r.addFinalizer(eventBus)
+	controllerutil.AddFinalizer(eventBus, finalizerName)
 
 	eventBus.Status.InitConditions()
 	return installer.Install(eventBus, r.client, r.natsStreamingImage, r.natsMetricsImage, log)
 }
 
-func (r *reconciler) addFinalizer(s *v1alpha1.EventBus) {
-	finalizers := sets.NewString(s.Finalizers...)
-	finalizers.Insert(finalizerName)
-	s.Finalizers = finalizers.List()
-}
-
-func (r *reconciler) removeFinalizer(s *v1alpha1.EventBus) {
-	finalizers := sets.NewString(s.Finalizers...)
-	finalizers.Delete(finalizerName)
-	s.Finalizers = finalizers.List()
-}
-
 func (r *reconciler) needsUpdate(old, new *v1alpha1.EventBus) bool {
 	if old == nil {
-		return true
-	}
-	if !equality.Semantic.DeepEqual(old.Status, new.Status) {
 		return true
 	}
 	if !equality.Semantic.DeepEqual(old.Finalizers, new.Finalizers) {
