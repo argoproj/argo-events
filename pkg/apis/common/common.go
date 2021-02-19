32e45d1803f0944aaf43ee005e057fbd344945a8
Derek Wang <whynowy@gmail.com>
2021-02-18 17:48:58 -0800

    test cases
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- pkg/apis/common/common.go
+++ pkg/apis/common/common.go
@@ -17,7 +17,6 @@ limitations under the License.
 package common
 
 import (
-	"errors"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
@@ -111,35 +110,6 @@ type TLSConfig struct {
 	// DeprecatedClientKeyPath refers the file path that contains client key.
 	// Deprecated: use ClientKeySecret instead
 	DeprecatedClientKeyPath string `json:"clientKeyPath" protobuf:"bytes,6,opt,name=clientKeyPath"`
-}
-
-// ValidateTLSConfig validates a TLS configuration.
-func ValidateTLSConfig(tlsConfig *TLSConfig) error {
-	if tlsConfig == nil {
-		return nil
-	}
-	var caCertSet, clientCertSet, clientKeySet bool
-
-	if tlsConfig.CACertSecret != nil || tlsConfig.DeprecatedCACertPath != "" {
-		caCertSet = true
-	}
-
-	if tlsConfig.ClientCertSecret != nil || tlsConfig.DeprecatedClientCertPath != "" {
-		clientCertSet = true
-	}
-
-	if tlsConfig.ClientKeySecret != nil || tlsConfig.DeprecatedClientKeyPath != "" {
-		clientKeySet = true
-	}
-
-	if !caCertSet && !clientCertSet && !clientKeySet {
-		return errors.New("invalid tls config, please configure either caCertSecret, or clientCertSecret and clientKeySecret, or both")
-	}
-
-	if (clientCertSet || clientKeySet) && (!clientCertSet || !clientKeySet) {
-		return errors.New("invalid tls config, both clientCertSecret and clientKeySecret need to be configured")
-	}
-	return nil
 }
 
 // Backoff for an operation
