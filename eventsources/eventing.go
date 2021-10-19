08ce9176ef5526c90dabf5941c93dbaf55de6fc3
daniel-codefresh <daniel.soifer@codefresh.io>
2021-10-19 13:37:31 +0300

    fixed e2e failures

--- eventsources/eventing.go
+++ eventsources/eventing.go
@@ -336,7 +336,6 @@ func (e *EventSourceAdaptor) run(ctx context.Context, servers map[apicommon.Even
 	cfConfig, err := codefresh.GetCodefreshConfig(ctx, namespace)
 	if err != nil {
 		logger.Errorw("failed to get Codefresh config", zap.Error(err))
-		return err
 	}
 
 	cctx, cancel := context.WithCancel(ctx)
@@ -429,12 +428,17 @@ func (e *EventSourceAdaptor) run(ctx context.Context, servers map[apicommon.Even
 							s.GetEventName()), zap.Any(logging.LabelEventSourceType, s.GetEventSourceType()), zap.String("eventID", event.ID()))
 						e.metrics.EventSent(s.GetEventSourceName(), s.GetEventName())
 
-						err = codefresh.ReportEventToCodefresh(eventBody, cfConfig)
-						if err != nil {
-							logger.Errorw("failed to report an event to Codefresh", zap.Error(err),
-								zap.String(logging.LabelEventName, s.GetEventName()), zap.Any(logging.LabelEventSourceType, s.GetEventSourceType()))
+						if cfConfig != nil {
+							err = codefresh.ReportEventToCodefresh(eventBody, cfConfig)
+							if err != nil {
+								logger.Errorw("failed to report an event to Codefresh", zap.Error(err),
+									zap.String(logging.LabelEventName, s.GetEventName()), zap.Any(logging.LabelEventSourceType, s.GetEventSourceType()))
+							} else {
+								logger.Infow("succeeded to report an event to Codefresh", zap.String(logging.LabelEventName, s.GetEventName()),
+									zap.Any(logging.LabelEventSourceType, s.GetEventSourceType()), zap.String("eventID", event.ID()))
+							}
 						} else {
-							logger.Infow("succeeded to report an event to Codefresh", zap.String(logging.LabelEventName, s.GetEventName()),
+							logger.Warnw("WARNING: skipping event reporting to Codefresh", zap.String(logging.LabelEventName, s.GetEventName()),
 								zap.Any(logging.LabelEventSourceType, s.GetEventSourceType()), zap.String("eventID", event.ID()))
 						}
 
