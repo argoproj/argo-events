39685ce23625bad7b8d576552c2261946a93a216
Daniel Soifer <daniel.soifer@codefresh.io>
2021-11-24 16:38:48 +0200

    feat: Added github app auth support to github eventsource
    Signed-off-by: Daniel Soifer <daniel.soifer@codefresh.io>

---
+++ eventsources/sources/github/appauth.go
@@ -0,0 +1,40 @@
+/*
+Copyright 2018 KompiTech GmbH
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package github
+
+import (
+	"net/http"
+
+	"github.com/bradleyfalzon/ghinstallation"
+)
+
+type AppsAuthStrategy struct {
+	AppID int64
+	InstallationID int64
+	PrivateKey string
+	Transport http.RoundTripper
+}
+
+// AuthTransport implements the AuthStrategy interface.
+func (t *AppsAuthStrategy) AuthTransport() (http.RoundTripper, error) {
+	return ghinstallation.New(t.transport(), t.AppID, t.InstallationID, []byte(t.PrivateKey))
+}
+
+func (t *AppsAuthStrategy) transport() http.RoundTripper {
+	if t.Transport != nil {
+		return t.Transport
+	}
+
+	return http.DefaultTransport
+}
