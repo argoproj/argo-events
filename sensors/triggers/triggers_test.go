9e076bc5c1b621cd32761cce915e49fd3cd7fd40
Daniel Soifer <daniel.soifer@codefresh.io>
2021-11-24 17:36:41 +0200

    fix: Added plural exception for eventbus resource for k8s trigger
    Signed-off-by: Daniel Soifer <daniel.soifer@codefresh.io>

--- sensors/triggers/triggers_test.go
+++ sensors/triggers/triggers_test.go
@@ -40,4 +40,12 @@ func TestGetGroupVersionResource(t *testing.T) {
 		Resource: "ingresses",
 	}
 	assert.Equal(t, expectedIngressGVR, GetGroupVersionResource(ingress))
+
+	eventbus := newUnstructured("argoproj.io/v1alpha1", "EventBus", "fake", "test-eb")
+	expectedEventBusGVR := schema.GroupVersionResource{
+		Group:    "argoproj.io/v1alpha1",
+		Version:  "v1",
+		Resource: "eventbus",
+	}
+	assert.Equal(t, expectedEventBusGVR, GetGroupVersionResource(eventbus))
 }
