faa69c73ac370c16226270fcf81b39a97472d48c
Daniel <69415974+daniel-codefresh@users.noreply.github.com>
2022-01-05 18:49:03 +0200

    Modified deprecated projectID field of Gitlab Eventsource to be optional (#1500)
    * modified deprecated projectID field of gitlab eventsource to be optional
    
    Signed-off-by: Daniel Soifer <daniel.soifer@codefresh.io>
    
    * modified deprecated projectID field of gitlab eventsource to be optional
    
    Signed-off-by: Daniel Soifer <daniel.soifer@codefresh.io>
    
    * fix: Modified deprecated projectID field of gitlab eventsource to be optional
    
    Signed-off-by: Daniel Soifer <daniel.soifer@codefresh.io>
    
    * codegen
    
    Signed-off-by: Daniel Soifer <daniel.soifer@codefresh.io>

--- pkg/apis/eventsource/v1alpha1/types.go
+++ pkg/apis/eventsource/v1alpha1/types.go
@@ -763,7 +763,8 @@ type GitlabEventSource struct {
 	Webhook *WebhookContext `json:"webhook,omitempty" protobuf:"bytes,1,opt,name=webhook"`
 	// DeprecatedProjectID is the id of project for which integration needs to setup
 	// Deprecated: use Projects instead. Will be unsupported in v 1.7
-	DeprecatedProjectID string `json:"projectID" protobuf:"bytes,2,opt,name=projectID"`
+	// +optional
+	DeprecatedProjectID string `json:"projectID,omitempty" protobuf:"bytes,2,opt,name=projectID"`
 	// Events are gitlab event to listen to.
 	// Refer https://github.com/xanzy/go-gitlab/blob/bf34eca5d13a9f4c3f501d8a97b8ac226d55e4d9/projects.go#L794.
 	Events []string `json:"events" protobuf:"bytes,3,opt,name=events"`
