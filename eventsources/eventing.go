16c653068d5cf49ab5ab41bfdf6b3deb2147e98c
daniel-codefresh <daniel.soifer@codefresh.io>
2022-01-12 19:12:35 +0200

    added another error reporting point

--- eventsources/eventing.go
+++ eventsources/eventing.go
@@ -440,7 +440,7 @@ func (e *EventSourceAdaptor) run(ctx context.Context, servers map[apicommon.Even
 								s.GetEventName()), zap.Any(logging.LabelEventSourceType, s.GetEventSourceType()))
 							e.metrics.EventSentFailed(s.GetEventSourceName(), s.GetEventName())
 							e.cfAPI.ReportError(
-								errors.Wrapf(err, "failed to publish an event [ %s: %s, %s: %s ]",
+								errors.Wrapf(err, "failed to publish an event { %s: %s, %s: %s }",
 									logging.LabelEventName, s.GetEventName(), logging.LabelEventSourceType, s.GetEventSourceType()),
 								codefresh.ErrorContext{
 									ObjectMeta: e.eventSource.ObjectMeta,
