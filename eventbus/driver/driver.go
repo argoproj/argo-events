fa02b013ae5d6ada9591069fb84206c5ee381590
Julie Vogelman <julie_vogelman@intuit.com>
2022-03-04 14:47:55 -0800

    clean up and minor changes
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- eventbus/driver/driver.go
+++ eventbus/driver/driver.go
@@ -12,17 +12,32 @@ import (
 type Driver interface {
 	Connect() (Connection, error)
 
-	// SubscribeEventSources is used to subscribe multiple event source dependencies
+	// SubscribeEventSources is used for a Sensor's Trigger to subscribe to multiple event source dependencies
 	// Parameter - ctx, context
 	// Parameter - conn, eventbus connection
 	// Parameter - group, NATS Streaming queue group or Kafka consumer group
+	// Parameter - sensorName, name of Sensor
+	// Parameter - triggerName, name of Trigger
 	// Parameter - closeCh, channel to indicate to close the subscription
+	// Parameter - lastResetTime, if there's a reset condition set, the previous time that it would have occurred (need to make sure we ignore any message from before that)
 	// Parameter - resetConditionsCh, channel to indicate to reset trigger conditions
 	// Parameter - dependencyExpr, example: "(dep1 || dep2) && dep3"
 	// Parameter - dependencies, array of dependencies information
 	// Parameter - filter, a function used to filter the message
 	// Parameter - action, a function to be triggered after all conditions meet
-	SubscribeEventSources(ctx context.Context, conn Connection, group string, closeCh <-chan struct{}, resetConditionsCh <-chan struct{}, lastResetTime time.Time, dependencyExpr string, dependencies []Dependency, transform func(depName string, event cloudevents.Event) (*cloudevents.Event, error), filter func(string, cloudevents.Event) bool, action func(map[string]cloudevents.Event)) error
+	SubscribeEventSources(ctx context.Context,
+		conn Connection,
+		group string,
+		sensorName string,
+		triggerName string,
+		closeCh <-chan struct{},
+		resetConditionsCh <-chan struct{},
+		lastResetTime time.Time,
+		dependencyExpr string,
+		dependencies []Dependency,
+		transform func(depName string, event cloudevents.Event) (*cloudevents.Event, error),
+		filter func(string, cloudevents.Event) bool,
+		action func(map[string]cloudevents.Event)) error
 
 	// Publish a message related to an event
 	Publish(conn Connection, message []byte, event Event) error
