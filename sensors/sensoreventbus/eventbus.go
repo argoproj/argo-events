d2e6a97c823612eda779af6e5daa6f122bcdb04d
Julie Vogelman <julie_vogelman@intuit.com>
2022-03-09 11:15:51 -0800

    sensoreventbus.Jetstream needs the Sensor spec so it can compare what's in the K/V store to the new spec and purge anything old
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- sensors/sensoreventbus/eventbus.go
+++ sensors/sensoreventbus/eventbus.go
@@ -9,6 +9,7 @@ import (
 	eventbusdriver "github.com/argoproj/argo-events/eventbus/driver"
 	apicommon "github.com/argoproj/argo-events/pkg/apis/common"
 	eventbusv1alpha1 "github.com/argoproj/argo-events/pkg/apis/eventbus/v1alpha1"
+	"github.com/argoproj/argo-events/pkg/apis/sensor/v1alpha1"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/pkg/errors"
 )
@@ -37,7 +38,7 @@ type Dependency struct {
 	EventName       string
 }
 
-func GetSensorDriver(ctx context.Context, eventBusConfig eventbusv1alpha1.BusConfig, sensorName string) (Driver, error) {
+func GetSensorDriver(ctx context.Context, eventBusConfig eventbusv1alpha1.BusConfig, sensorSpec *v1alpha1.Sensor) (Driver, error) {
 	auth, err := eventbus.GetAuth(ctx, eventBusConfig)
 	if err != nil {
 		return nil, err
@@ -56,9 +57,9 @@ func GetSensorDriver(ctx context.Context, eventBusConfig eventbusv1alpha1.BusCon
 	var dvr Driver
 	switch eventBusType {
 	case apicommon.EventBusNATS:
-		dvr = NewNATSStreaming(eventBusConfig.NATS.URL, *eventBusConfig.NATS.ClusterID, sensorName, auth, logger)
+		dvr = NewNATSStreaming(eventBusConfig.NATS.URL, *eventBusConfig.NATS.ClusterID, sensorSpec.Name, auth, logger)
 	//case apicommon.EventBusJetstream:
-	//	dvr = NewJetstream(eventBusConfig.Jetstream.URL, sensorName, auth, logger) // don't need to pass in subject because subjects will be derived from dependencies
+	//	dvr = NewJetstream(eventBusConfig.Jetstream.URL, sensorSpec, auth, logger) // don't need to pass in subject because subjects will be derived from dependencies
 	default:
 		return nil, errors.New("invalid eventbus type")
 	}
