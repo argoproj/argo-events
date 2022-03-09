77ce527eac9cd60445cb9ec3bb48544a9576de45
Julie Vogelman <julie_vogelman@intuit.com>
2022-03-08 17:03:52 -0800

    incorporating the jetstream controller changes from master
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- eventsources/sourceeventbus/eventbus.go
+++ eventsources/sourceeventbus/eventbus.go
@@ -35,8 +35,8 @@ func GetSourceDriver(ctx context.Context, eventBusConfig eventbusv1alpha1.BusCon
 	var eventBusType apicommon.EventBusType
 	if eventBusConfig.NATS != nil {
 		eventBusType = apicommon.EventBusNATS
-	} else if eventBusConfig.Jetstream != nil {
-		eventBusType = apicommon.EventBusJetstream
+	} else if eventBusConfig.JetStream != nil {
+		eventBusType = apicommon.EventBusJetStream
 	} else {
 		return nil, errors.New("invalid event bus")
 	}
@@ -45,8 +45,8 @@ func GetSourceDriver(ctx context.Context, eventBusConfig eventbusv1alpha1.BusCon
 	switch eventBusType {
 	case apicommon.EventBusNATS:
 		dvr = NewNATSStreaming(eventBusConfig.NATS.URL, *eventBusConfig.NATS.ClusterID, eventSourceName, hostname, defaultSubject, auth, logger)
-	case apicommon.EventBusJetstream:
-		dvr = NewJetstream(eventBusConfig.Jetstream.URL, eventSourceName, auth, logger) // don't need to pass in subject because subjects will be derived from dependencies
+	case apicommon.EventBusJetStream:
+		dvr = NewJetstream(eventBusConfig.JetStream.URL, eventSourceName, auth, logger) // don't need to pass in subject because subjects will be derived from dependencies
 	default:
 		return nil, errors.New("invalid eventbus type")
 	}
