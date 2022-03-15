035cdf2771048f5c71709337b7f9cfa4e727a2df
Julie Vogelman <julie_vogelman@intuit.com>
2022-03-15 14:50:40 -0700

    successful trigger for conditional expression not involving &&
    Signed-off-by: Julie Vogelman <julie_vogelman@intuit.com>

--- sensors/sensoreventbus/jetstreamTriggerConn.go
+++ sensors/sensoreventbus/jetstreamTriggerConn.go
@@ -3,6 +3,7 @@ package sensoreventbus
 import (
 	"context"
 	"fmt"
+	"strings"
 	"sync"
 	"time"
 
@@ -22,6 +23,7 @@ type JetstreamTriggerConn struct {
 	triggerName          string
 	keyValueStore        *nats.KeyValue
 	dependencyExpression string
+	requiresANDLogic     bool
 	deps                 []Dependency
 	sourceDepMap         map[string][]string // maps EventSource and EventName to dependency name
 }
@@ -33,16 +35,24 @@ func NewJetstreamTriggerConn(conn *eventbusdriver.JetstreamConnection,
 	dependencyExpression string,
 	deps []Dependency) *JetstreamTriggerConn {
 
-	srcDepMap := make(map[string][]string)
+	sourceDepMap := make(map[string][]string)
 	for _, d := range deps {
 		key := d.EventSourceName + "__" + d.EventName
-		_, found := srcDepMap[key]
+		_, found := sourceDepMap[key]
 		if !found {
-			srcDepMap[key] = make([]string, 0)
+			sourceDepMap[key] = make([]string, 0)
 		}
-		srcDepMap[key] = append(srcDepMap[key], d.Name)
+		sourceDepMap[key] = append(sourceDepMap[key], d.Name)
 	}
-	connection := &JetstreamTriggerConn{conn, sensorName, triggerName, keyValueStore, dependencyExpression, deps, srcDepMap}
+	connection := &JetstreamTriggerConn{
+		JetstreamConnection:  conn,
+		sensorName:           sensorName,
+		triggerName:          triggerName,
+		keyValueStore:        keyValueStore,
+		dependencyExpression: dependencyExpression,
+		requiresANDLogic:     strings.Contains(dependencyExpression, "&"),
+		deps:                 deps,
+		sourceDepMap:         sourceDepMap}
 	connection.Logger = connection.Logger.With("triggerName", connection.triggerName).With("clientID", connection.ClientID())
 	return connection
 }
@@ -156,7 +166,7 @@ func (conn *JetstreamTriggerConn) processMsgs(
 	for {
 		select {
 		case msg := <-receiveChannel:
-			conn.processMsg(msg)
+			conn.processMsg(msg, transform, filter, action)
 		case <-closeCh:
 			conn.Logger.Info("shutting down processMsgs routine")
 			wg.Done()
@@ -165,30 +175,82 @@ func (conn *JetstreamTriggerConn) processMsgs(
 	}
 }
 
-func (conn *JetstreamTriggerConn) processMsg(m *nats.Msg) {
-	defer m.Ack()
+func (conn *JetstreamTriggerConn) processMsg(
+	m *nats.Msg,
+	transform func(depName string, event cloudevents.Event) (*cloudevents.Event, error),
+	filter func(string, cloudevents.Event) bool,
+	action func(map[string]cloudevents.Event)) {
+
+	defer m.Ack() // todo: how do we do Exactly once delivery?:
+	// Documentation says: "Consumers can be 100% sure a message was correctly processed by "
+	// requesting the server Acknowledge having received your acknowledgement by setting a reply
+	// subject on the Ack. If you receive this response you will never receive that message again."
 	log := conn.Logger
-	//conn.Logger.Infof("received new message: %+v", msg)
 
 	var event *cloudevents.Event
 	if err := json.Unmarshal(m.Data, &event); err != nil {
 		log.Errorf("Failed to convert to a cloudevent, discarding it... err: %v", err)
-		_ = m.Ack()
 		return
 	}
 
-	// get the dependency name from the event source name and event name
+	// get all dependencies for this Trigger that match
 	depNames, err := conn.getDependencyNames(event.Source(), event.Subject())
-	if err != nil {
+	if err != nil || len(depNames) == 0 {
 		log.Errorf("Failed to get the dependency names, discarding it... err: %v", err)
-		_ = m.Ack()
 		return
 	}
 
 	log.Debugf("New incoming Event Source Message, dependency names=%s", depNames)
 
-	// get all dependencies for this Trigger that match
+	for _, depName := range depNames {
+		conn.processDependency(m, event, depName, transform, filter, action)
+	}
+}
 
+func (conn *JetstreamTriggerConn) processDependency(
+	m *nats.Msg,
+	event *cloudevents.Event,
+	depName string,
+	transform func(depName string, event cloudevents.Event) (*cloudevents.Event, error),
+	filter func(string, cloudevents.Event) bool,
+	action func(map[string]cloudevents.Event)) {
+
+	log := conn.Logger
+	event, err := transform(depName, *event)
+	if err != nil {
+		log.Errorw("failed to apply event transformation, ", err)
+		return
+	}
+
+	if !filter(depName, *event) {
+		// message not interested
+		log.Debugf("not interested in dependency %s (didn't pass filter)", depName)
+		return
+	}
+
+	if !conn.requiresANDLogic {
+		// this is the simple case: we can just perform the trigger
+		messages := make(map[string]cloudevents.Event)
+		messages[depName] = *event
+		log.Infof("Triggering actions after receiving dependency %s", depName)
+
+		action(messages)
+	} else {
+		// check Dependency expression (need to retrieve previous dependencies from Key/Value store)
+
+		parameters := make(map[string]bool, len(conn.deps))
+		//messages := conn.getSavedDependencies()
+		// derive parameters from messages
+
+		parameters[depName] = true
+
+		// if expression is true, trigger and clear the K/V store
+		// else save the new message in the K/V store
+	}
+}
+
+func (conn *JetstreamTriggerConn) getSavedDependencies() map[string]cloudevents.Event {
+	return nil
 }
 
 func (conn *JetstreamTriggerConn) getDependencyNames(eventSourceName, eventName string) ([]string, error) {
@@ -211,4 +273,12 @@ func (conn *JetstreamTriggerConn) CleanUpOnStart() error {
 	// - messages sent to that Trigger that never reached it and are waiting in the eventbus (need to make a new connection and Drain())
 
 	return nil
+}
+
+type msgInfo struct {
+	seq       uint64
+	timestamp int64
+	event     *cloudevents.Event
+	// timestamp of last delivered
+	lastDeliveredTime int64
 }
