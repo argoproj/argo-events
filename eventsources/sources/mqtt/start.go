cc364245b9032860a3f10a8ab643e2e972615843
Derek Wang <whynowy@gmail.com>
2022-03-22 22:15:55 -0700

    rename
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- eventsources/sources/mqtt/start.go
+++ eventsources/sources/mqtt/start.go
@@ -59,7 +59,7 @@ func (el *EventListener) GetEventSourceType() apicommon.EventSourceType {
 }
 
 // StartListening starts listening events
-func (el *EventListener) StartListening(ctx context.Context, dispatch func([]byte, ...eventsourcecommon.Options) error) error {
+func (el *EventListener) StartListening(ctx context.Context, dispatch func([]byte, ...eventsourcecommon.Option) error) error {
 	log := logging.FromContext(ctx).
 		With(logging.LabelEventSourceType, el.GetEventSourceType(), logging.LabelEventName, el.GetEventName())
 	defer sources.Recover(el.GetEventName())
