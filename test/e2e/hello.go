6c409970369639122c386e23aa997089f7339e9d
Daniel Haimov <daniel.haimov@codefresh.io>
2022-06-13 12:03:32 +0300

    added simple e2e test

---
+++ test/e2e/hello.go
@@ -0,0 +1,6 @@
+package e2e
+
+func Hello() string {
+	return "Hello, world"
+}
+
---
+++ test/e2e/hello_test.go
@@ -0,0 +1,12 @@
+package e2e
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, world"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
