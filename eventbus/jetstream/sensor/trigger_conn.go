33c7a35c251f2b0ce69c0bbe7cc57f545c58d4db
Bilal Bakht Ahmad <bilalba@users.noreply.github.com>
2023-01-17 13:41:49 -0800

    Implement optional at least once semantics (#2404)
    Signed-off-by: Bilal Bakht Ahmad <tringingly@gmail.com>
    Signed-off-by: David Farr <david_farr@intuit.com>
    Signed-off-by: Prema devi Kuppuswamy <premadk@gmail.com>

--- eventbus/jetstream/sensor/trigger_conn.go
+++ eventbus/jetstream/sensor/trigger_conn.go
@@ -257,15 +257,35 @@ func (conn *JetstreamTriggerConn) processMsg(
 		conn.Logger.Errorf("can't get Metadata() for message %+v??", m)
 	}
 
-	defer func() {
-		err = m.AckSync() // this call does the acknowledgment and waits for an acknowledgment from the server (that it received the ack) too
-		if err != nil {
-			errStr := fmt.Sprintf("Error performing AckSync() on message: %v", err)
-			conn.Logger.Error(errStr)
+	done := make(chan bool)
+	go func() {
+		ticker := time.NewTicker(500 * time.Millisecond)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-done:
+				err = m.AckSync()
+				if err != nil {
+					errStr := fmt.Sprintf("Error performing AckSync() on message: %v", err)
+					conn.Logger.Error(errStr)
+				}
+				conn.Logger.Debugf("acked message of Stream seq: %s:%d, Consumer seq: %s:%d", meta.Stream, meta.Sequence.Stream, meta.Consumer, meta.Sequence.Consumer)
+				return
+			case <-ticker.C:
+				err = m.InProgress()
+				if err != nil {
+					errStr := fmt.Sprintf("Error performing InProgess() on message: %v", err)
+					conn.Logger.Error(errStr)
+				}
+				conn.Logger.Debugf("InProgess message of Stream seq: %s:%d, Consumer seq: %s:%d", meta.Stream, meta.Sequence.Stream, meta.Consumer, meta.Sequence.Consumer)
+			}
 		}
-
-		conn.Logger.Debugf("acked message of Stream seq: %s:%d, Consumer seq: %s:%d", meta.Stream, meta.Sequence.Stream, meta.Consumer, meta.Sequence.Consumer)
 	}()
+
+	defer func() {
+		done <- true
+	}()
+
 	log := conn.Logger
 
 	var event *cloudevents.Event
