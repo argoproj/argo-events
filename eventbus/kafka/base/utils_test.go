d6e1b08b951739b47acf46c3ae895aa50db04426
David Farr <david_farr@intuit.com>
2023-03-01 21:26:50 -0800

    Add timing asserts to batch tests
    Signed-off-by: David Farr <david_farr@intuit.com>

--- eventbus/kafka/base/utils_test.go
+++ eventbus/kafka/base/utils_test.go
@@ -13,17 +13,23 @@ func TestBatchDurationReached(t *testing.T) {
 
 	out := Batch(5, 1*time.Second, in)
 
+	t0 := time.Now()
 	in <- 0
 	assert.Equal(t, []int{0}, <-out)
+	assert.Equal(t, time.Second, time.Since(t0).Truncate(time.Second))
 
+	t1 := time.Now()
 	in <- 1
 	in <- 2
 	assert.Equal(t, []int{1, 2}, <-out)
+	assert.Equal(t, time.Second, time.Since(t1).Truncate(time.Second))
 
+	t2 := time.Now()
 	in <- 3
 	in <- 4
 	in <- 5
 	assert.Equal(t, []int{3, 4, 5}, <-out)
+	assert.Equal(t, time.Second, time.Since(t2).Truncate(time.Second))
 }
 
 func TestBatchSizeReached(t *testing.T) {
@@ -32,16 +38,20 @@ func TestBatchSizeReached(t *testing.T) {
 
 	out := Batch(2, 1*time.Second, in)
 
+	t0 := time.Now()
 	in <- 0
 	in <- 1
 	assert.Equal(t, <-out, []int{0, 1})
+	assert.Equal(t, time.Duration(0), time.Since(t0).Truncate(time.Second))
 
+	t1 := time.Now()
 	in <- 2
 	in <- 3
 	in <- 4
 	in <- 5
 	assert.Equal(t, []int{2, 3}, <-out)
 	assert.Equal(t, []int{4, 5}, <-out)
+	assert.Equal(t, time.Duration(0), time.Since(t1).Truncate(time.Second))
 }
 
 func TestBatchMaintainsOrder(t *testing.T) {
