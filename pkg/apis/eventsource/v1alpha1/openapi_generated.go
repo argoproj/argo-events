7ee9f8b0c168c754df7ddf5821f9971b646a9013
sysr9 <38893296+sysr9@users.noreply.github.com>
2024-05-21 23:34:45 +0300

    Optional use QueueSubscribe for NATS events source (#3131)
    Signed-off-by: sysr9 <sysr999@gmail.com>

--- pkg/apis/eventsource/v1alpha1/openapi_generated.go
+++ pkg/apis/eventsource/v1alpha1/openapi_generated.go
@@ -2962,6 +2962,13 @@ func schema_pkg_apis_eventsource_v1alpha1_NATSEventsSource(ref common.ReferenceC
 							Ref:         ref("github.com/argoproj/argo-events/pkg/apis/eventsource/v1alpha1.EventSourceFilter"),
 						},
 					},
+					"queue": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Queue is the name of the queue group to subscribe as if specified. Uses QueueSubscribe logic to subscribe as queue group. If the queue is empty, uses default Subscribe logic.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
 				},
 				Required: []string{"url", "subject"},
 			},
