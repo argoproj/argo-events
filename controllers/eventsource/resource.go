ce2eeaf0e04bd5e97b1de4c242cceae30b4c6d75
MenD32 <amit@jounce.io>
2024-08-24 20:31:36 +0300

    debugging kafka e2e
    Signed-off-by: MenD32 <amit@jounce.io>

--- controllers/eventsource/resource.go
+++ controllers/eventsource/resource.go
@@ -385,22 +385,18 @@ func buildService(args *AdaptorArgs) (*corev1.Service, error) {
 	ports := []corev1.ServicePort{}
 	ports = append(ports, eventSource.Spec.Service.Ports...)
 	labels := mergeLabels(args.EventSource.Labels, args.Labels)
-	// annotations := make(map[string]string)
+	annotations := make(map[string]string)
 
 	if args.EventSource.Spec.Service.Metadata != nil {
 		labels = mergeLabels(labels, args.EventSource.Spec.Service.Metadata.Labels)
-		// annotations = args.EventSource.Spec.Service.Metadata.Labels
+		annotations = args.EventSource.Spec.Service.Metadata.Labels
 	}
 
 	svc := &corev1.Service{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("%s-eventsource-svc", eventSource.Name),
-			Namespace: eventSource.Namespace,
-			Labels:    mergeLabels(args.EventSource.Labels, args.Labels),
-			// Name:        fmt.Sprintf("%s-eventsource-svc", eventSource.Name),
-			// Namespace:   eventSource.Namespace,
-			// Labels:      labels,
-			// Annotations: annotations,
+			Name:        fmt.Sprintf("%s-eventsource-svc", eventSource.Name),
+			Namespace:   eventSource.Namespace,
+			Labels:      labels,
 		},
 		Spec: corev1.ServiceSpec{
 			Ports:     ports,
@@ -412,6 +408,7 @@ func buildService(args *AdaptorArgs) (*corev1.Service, error) {
 	if err := controllerscommon.SetObjectMeta(eventSource, svc, v1alpha1.SchemaGroupVersionKind); err != nil {
 		return nil, err
 	}
+	svc.SetAnnotations(mergeLabels(annotations ,svc.GetObjectMeta().GetAnnotations()))
 	return svc, nil
 }
 
