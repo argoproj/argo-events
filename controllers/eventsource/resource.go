4589166d55d5a96de7ee1863f0f5c944785be51e
MenD32 <amit@jounce.io>
2024-09-04 15:47:39 +0300

    debugging
    Signed-off-by: MenD32 <amit@jounce.io>

--- controllers/eventsource/resource.go
+++ controllers/eventsource/resource.go
@@ -405,6 +405,7 @@ func buildService(args *AdaptorArgs) (*corev1.Service, error) {
 		// }
 		// if args.EventSource.Spec.Service.Metadata.Annotations != nil {
 		svc.ObjectMeta.SetAnnotations(args.EventSource.Spec.Service.Metadata.Annotations)
+		
 		// 	}
 	}
 
