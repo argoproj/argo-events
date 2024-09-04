eb0ed9e1833cb3a1fc8c031e2d80f51a748a95ba
MenD32 <amit@jounce.io>
2024-09-04 15:21:31 +0300

    debugging
    Signed-off-by: MenD32 <amit@jounce.io>

--- controllers/eventsource/resource.go
+++ controllers/eventsource/resource.go
@@ -401,10 +401,7 @@ func buildService(args *AdaptorArgs) (*corev1.Service, error) {
 
 	svc.ObjectMeta.SetAnnotations(args.EventSource.Spec.Service.Metadata.Annotations)
 	svc.ObjectMeta.SetLabels(args.EventSource.Spec.Service.Metadata.Labels)
-	// if args.EventSource.Spec.Service.Metadata.Annotations != nil {
-	// }
-	// if args.EventSource.Spec.Service.Metadata.Labels != nil {
-	// }
+	
 	if err := controllerscommon.SetObjectMeta(eventSource, svc, v1alpha1.SchemaGroupVersionKind); err != nil {
 		return nil, err
 	}
