a0b03eb77cfe42b83ccf1ae02d9b41f976ee18cf
MenD32 <amit@jounce.io>
2024-09-04 15:14:55 +0300

    fixed lint
    Signed-off-by: MenD32 <amit@jounce.io>

--- controllers/eventsource/resource.go
+++ controllers/eventsource/resource.go
@@ -399,11 +399,12 @@ func buildService(args *AdaptorArgs) (*corev1.Service, error) {
 		},
 	}
 
-if args.EventSource.Spec.Service.Metadata.Annotations != nil {
-  svc.ObjectMeta.SetAnnotations(args.EventSource.Spec.Service.Metadata.Annotations)
-}
-if args.EventSource.Spec.Service.Metadata.Labels != nil {	svc.ObjectMeta.SetLabels(args.EventSource.Spec.Service.Metadata.Labels)
-}
+	if args.EventSource.Spec.Service.Metadata.Annotations != nil {
+  		svc.ObjectMeta.SetAnnotations(args.EventSource.Spec.Service.Metadata.Annotations)
+	}
+	if args.EventSource.Spec.Service.Metadata.Labels != nil {
+		svc.ObjectMeta.SetLabels(args.EventSource.Spec.Service.Metadata.Labels)
+	}
 	if err := controllerscommon.SetObjectMeta(eventSource, svc, v1alpha1.SchemaGroupVersionKind); err != nil {
 		return nil, err
 	}
