e91f6763ebb29dae353203479fe85352039f61c0
Derek Wang <whynowy@gmail.com>
2024-09-19 19:53:05 -0700

    .
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- hack/tools.go
+++ hack/tools.go
@@ -13,6 +13,7 @@ import (
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "golang.org/x/tools/cmd/goimports"
 	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/conversion-gen"
