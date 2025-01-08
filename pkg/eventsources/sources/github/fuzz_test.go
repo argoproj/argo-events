82f0ac1c1263f50f73a4500f14e39839ab0aa234
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2025-01-08 07:29:20 +0000

    move fuzz tests upstream from cncf-fuzzing (#3414)
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

---
+++ pkg/eventsources/sources/github/fuzz_test.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2025 The Argoproj Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package github
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/argoproj/argo-events/pkg/eventsources/common/webhook"
+
+	"github.com/argoproj/argo-events/pkg/apis/events/v1alpha1"
+)
+
+func FuzzGithubEventsource(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fRoute := webhook.GetFakeRoute()
+		router := &Router{
+			route:             fRoute,
+			githubEventSource: &v1alpha1.GithubEventSource{},
+		}
+		router.route.Active = true
+		writer := &webhook.FakeHttpWriter{}
+		r := &http.Request{
+			Body: io.NopCloser(bytes.NewReader(data)),
+		}
+		r.Header = make(map[string][]string)
+		r.Header.Set("Content-Type", "application/json")
+		router.HandleRoute(writer, r)
+	})
+}
---
+++ pkg/reconciler/sensor/fuzz_test.go
@@ -0,0 +1,97 @@
+/*
+Copyright 2025 The Argoproj Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sensor
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
+	appv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	eventbusv1alpha1 "github.com/argoproj/argo-events/pkg/apis/events/v1alpha1"
+	"github.com/argoproj/argo-events/pkg/shared/logging"
+)
+
+var initter sync.Once
+
+func initFunc() {
+	_ = eventbusv1alpha1.AddToScheme(scheme.Scheme)
+	_ = appv1.AddToScheme(scheme.Scheme)
+	_ = corev1.AddToScheme(scheme.Scheme)
+}
+
+func FuzzSensorControllerreconcile(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		initter.Do(initFunc)
+		testImage := "test-image"
+		f := fuzz.NewConsumer(data)
+		sensorObj := &eventbusv1alpha1.Sensor{}
+		err := f.GenerateStruct(sensorObj)
+		if err != nil {
+			return
+		}
+		ctx := context.Background()
+		cl := fake.NewClientBuilder().Build()
+		r := &reconciler{
+			client:      cl,
+			scheme:      scheme.Scheme,
+			sensorImage: testImage,
+			logger:      logging.NewArgoEventsLogger(),
+		}
+		_ = r.reconcile(ctx, sensorObj)
+	})
+}
+
+func FuzzSensorControllerReconcile(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		initter.Do(initFunc)
+		testImage := "test-image"
+		f := fuzz.NewConsumer(data)
+		testBus := &eventbusv1alpha1.EventBus{}
+		err := f.GenerateStruct(testBus)
+		if err != nil {
+			return
+		}
+		sensorObj := &eventbusv1alpha1.Sensor{}
+		err = f.GenerateStruct(sensorObj)
+		if err != nil {
+			return
+		}
+		testLabels := make(map[string]string)
+		err = f.FuzzMap(&testLabels)
+		if err != nil {
+			return
+		}
+		ctx := context.Background()
+		cl := fake.NewClientBuilder().Build()
+		err = cl.Create(ctx, testBus)
+		if err != nil {
+			return
+		}
+		args := &AdaptorArgs{
+			Image:  testImage,
+			Sensor: sensorObj,
+			Labels: testLabels,
+		}
+		_ = Reconcile(cl, nil, args, logging.NewArgoEventsLogger())
+	})
+}
