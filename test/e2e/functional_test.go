1198b816da27260bff4ea2002e1969faeadd6fa6
Eduardo Rodrigues <eduardodbr@hotmail.com>
2025-11-20 19:48:08 +0000

    chore: revert test change and add sleep
    Signed-off-by: Eduardo Rodrigues <eduardodbr@hotmail.com>

--- test/e2e/functional_test.go
+++ test/e2e/functional_test.go
@@ -204,14 +204,6 @@ func (s *FunctionalSuite) TestMetricsWithWebhook() {
 }
 
 func (s *FunctionalSuite) TestResourceEventSource() {
-	t2 := s.Given().Sensor("@testdata/sensor-resource.yaml").
-		When().
-		CreateSensor().
-		WaitForSensorReady().
-		Then().
-		ExpectSensorPodLogContains(LogSensorStarted)
-	defer t2.When().DeleteSensor()
-
 	w1 := s.Given().EventSource("@testdata/es-resource.yaml").
 		When().
 		CreateEventSource().
@@ -221,6 +213,16 @@ func (s *FunctionalSuite) TestResourceEventSource() {
 	t1 := w1.Then().
 		ExpectEventSourcePodLogContains(LogEventSourceStarted)
 	defer t1.When().DeleteEventSource()
+
+	t2 := s.Given().Sensor("@testdata/sensor-resource.yaml").
+		When().
+		CreateSensor().
+		WaitForSensorReady().
+		Then().
+		ExpectSensorPodLogContains(LogSensorStarted)
+	defer t2.When().DeleteSensor()
+
+	time.Sleep(time.Second * 2)
 
 	w1.Exec("kubectl", []string{"-n", fixtures.Namespace, "delete", "pod", "test-pod"}, fixtures.OutputRegexp(`pod "test-pod" deleted`))
 
